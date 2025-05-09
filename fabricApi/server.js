3ec60f899b482f6be768c2326d8abffe9a07a869
ThaoTo2806 <thaonguyen28062003@gmail.com>
2025-05-09 09:56:29 +0700

    version 3

--- fabricApi/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-const PORT = process.env.PORT || 3001;
-require("dotenv").config();
-const { enrollAdmin } = require("./services/adminService");
-
-enrollAdmin()
-app.listen(PORT, () => console.log(`API running on port ${PORT}`));
